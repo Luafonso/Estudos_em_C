af890824aeb62e0759db6cced8c6f74c9b27ea3f
Luafonso <zorzettoafonso11@gmail.com>
2023-02-28 14:55:14 -0300

    Adiciona exercicio 7

---
+++ exercicio7/exercicio7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    /*
+     * Luis Afonso
+     * 28/02/2023
+     * 14:53hrs
+     * */
+    printf("Olá, sou Luis Afonso\n");
+    printf("Tenho 18 anos\n");
+    printf("Sou do Brasil\n");
+    return 0;
+}
