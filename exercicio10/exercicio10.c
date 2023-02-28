c70c997d36a03feedf7336a20563b0316d749e6b
Luafonso <zorzettoafonso11@gmail.com>
2023-02-28 15:50:22 -0300

    Adicionar o exercicio 10

---
+++ exercicio10/exercicio10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("\tNUM lugar da mancha, de cujo nome nao quero lembrar-me, vivia, nao\n");
+    printf("ha muito, um fidalgo, dos de lança em cabido, adarga antiga, rocim\n");
+    printf("fraco, e galgo corredor.\n");
+    printf("\n");
+    printf("\tPassadio, olha seu tanto mais de vaca do que de carneiro,as mais\n");
+    printf("das ceias restos da carne picados com sua cebola e vinagre, aos sabados\n");
+    printf("outros sobejos ainda somenos, lentilhas as sextas-feiras, algum pombito\n");
+    printf("de crescenca aos domingos, consumiam tres quartos do seu haver. O\n");
+    printf("remanescente, levavam-no saio de belarte, calcas de veludo para as\n");
+    printf("festas, com seus pantufos do mesmo; e para os dias de semana o seu\n");
+    printf("bellori do mais fino.\n");
+    printf("\n");
+    printf("(Dom Quichote de La Mancha - Cervantes)");
+    return 0;
+}
