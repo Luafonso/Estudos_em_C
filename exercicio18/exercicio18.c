8442988f3532288be33d1f6797a6e5555060a780
Luafonso <zorzettoafonso11@gmail.com>
2023-03-11 14:48:15 -0300

    Retira os arquivos duplicados

--- exercicio18/exercicio18.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Luis Afonso Zorzetto Laurindo\n");
-    return 0;
-}
--- exercicio19/exercicio19.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("28 * 43 = %d\n", 28*43);
-    return 0;
-}
