61156db4c6c2469e0c3e2eb1f0eaf82018c8c554
Luafonso <zorzettoafonso11@gmail.com>
2023-04-16 19:55:50 -0300

    Adiciona novos exercícios

---
+++ Lista2deExercicios/exercicio12/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+	int num3;
+	int maior;
+	int intermediario;
+	int menor;
+
+	printf("Digite o valor do primeiro número: \n");
+	scanf("%d", &num1);
+	printf("Digite o valor do segundo número: \n");
+	scanf("%d", &num2);
+	printf("Digite o valor do terceiro número: \n");
+	scanf("%d", &num3);
+	
+	if(num1 > num2 && num1 > num3){
+		maior = num1;
+		if(num2 > num3){
+			intermediario = num2;
+			menor = num3;
+		}
+		else{
+			intermediario = num3;
+			menor = num2;
+		}
+	}
+	else if (num2 > num1 && num2 > num3){
+		maior = num2;
+		if(num1 > num3){
+			intermediario = num1;
+			menor = num3;
+		}
+		else{
+			intermediario = num3;
+			menor = num1;
+		}
+	}
+	else{
+		maior = num3; 
+		if(num1 > num2){
+			intermediario = num1;
+			menor = num2;
+		}
+		else{
+			intermediario = num2;
+			menor = num1;
+		}
+	}
+	printf("---------------------------------\n");
+	printf("O valor de maior é: %d\n", maior);
+	printf("O valor de intermediario é: %d\n", intermediario);
+	printf("O valor de menor é: %d\n", menor);
+	return 0;
+}
+
